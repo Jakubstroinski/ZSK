9e3d64cf0c62fc534e6eac44727b83eaaad6107b
Jakub Stroiński <62056684+BuJax11@users.noreply.github.com>
2020-09-09 09:33:18 +0200

    Add files via upload
    Second Commit

--- 1_podstawy.php
+++ 1_podstawy.php
@@ -1,3 +1,9 @@
 <?php
-  echo test
+  $name = 'Janusz';
+  $lastName = 'Skompski';
+  echo 'Imie: $name<br>';
+  echo " Imie: $name<br>";
+  // konkatenacja w php to '.'
+  echo 'Nazwisko: $lastName'.'<br>';
+  echo " Nazwisko: $lastName";
  ?>
